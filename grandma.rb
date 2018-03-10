080881faca32fa6f3a2c993321038daeecc1cf2c
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-10 00:40:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,11 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 def speak_to_grandma(string)
-  if string == string.upcase
-    return "NO, NOT SINCE 1938!"
-  elsif string == "I LOVE YOU GRANDMA!"
+  
+  if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif string == string.upcase
+      return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
