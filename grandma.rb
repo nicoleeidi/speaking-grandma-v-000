bca755cd01db4536b054e90307dc689a731938db
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-10 00:40:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,16 @@
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
+def speak_to_grandma(string)
+  if string == string.upcase 
+    return "NO, NOT SINCE 1938!"
+  elsif string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  else 
+    return "HUH?! SPEAK UP, SONNY!"
+  end 
+end 
+    
 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
